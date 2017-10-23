e0bae22fa8d1c842d18eff4420201ed256e0de61
Peter <peterhj@gmail.com>
2017-10-24 01:13:06 +0200

    try parallel again

--- main.nim
+++ main.nim
@@ -10,7 +10,7 @@ randomize()
 # Globals
 const N_THREADS = 4
 const POPULATION_SIZE = 10_000_000
-const N_GENERATIONS = 1_000_000
+const N_GENERATIONS = 1000
 const NUM_CHARACTERS = 1000
 const TARGET = @[0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
 const MUTATE_PROP = 0.10
@@ -116,14 +116,14 @@ proc partition(pool: seq[Agent], n: int): seq[seq[Agent]] =
             idx += 1
     # echo($$ result)
 
-proc life(part: ptr seq[Agent], pool: ptr seq[Agent], wheel: ptr seq[float]) =
+proc life(part: seq[Agent], pool: seq[Agent], wheel: seq[float]) =
     for a in part:
         if random(1.0) < CROSSOVER_PROP:
-            a.crossover(pool[], wheel[])
-            #a.crossover(pool, wheel)
+            a.crossover(pool, wheel)
         if random(1.0) < MUTATE_PROP:
             a.mutate()
         a.calcFitness()
+    echo "life done"
 
 proc execute() =
     if (POPULATION_SIZE mod N_THREADS != 0):
@@ -134,16 +134,16 @@ proc execute() =
     for gen in 0..<N_GENERATIONS:
         let wheel = createWheel(pool)
         let partitions = partition(pool, N_THREADS)
-        for part in partitions:
-            spawn life(cast[ptr seq[Agent]](part), cast[ptr seq[Agent]](pool), cast[ptr seq[float]](wheel))
-        sync()
+        parallel:
+            for part in partitions:
+                echo "spawning"
+                spawn life(part, pool, wheel)
         pool = createPool(pool, wheel)
         
-        if gen mod 100 == 0:
-            echo("Generation, ", gen, "/", N_GENERATIONS)
-            let best = maxAgent(pool)        
-            stdout.write("best: ")
-            best.print()
+        echo("Generation, ", gen, "/", N_GENERATIONS)
+        let best = maxAgent(pool)        
+        stdout.write("best: ")
+        best.print()
     
     
             
--- recreateProblem.nim
+++ recreateProblem.nim
@@ -4,9 +4,9 @@ import os
 import strutils, math, threadpool
 import times
 
-const POOLSIZE = 5
-const NUM_CHARACTERS = 1000
-const TARGET = @[0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
+const NUM_CHARACTERS = 10
+const TARGET = @[0, 0, 1, 1, 2, 2]
+
 
 type
     Agent* = ref object
@@ -14,31 +14,23 @@ type
         fitness*: float
 
 proc newAgent(): Agent =
-    sleep(1000)
     result = Agent()
     result.fitness = -1.0
     result.data = newSeq[int](TARGET.len)
     for i in 0..<TARGET.len:
         result.data[i] = random(NUM_CHARACTERS)
 
-method calcFitness(this: Agent) {.base.} =
-    this.fitness = 0.0
-    for i in 0..<TARGET.len:
-        if this.data[i] == TARGET[i]:
-            this.fitness += 1.0
-    this.fitness *= this.fitness
+proc mutate(s: seq[Agent]) =
+    s[0].fitness = 100
 
 proc main() =
-    echo getClockStr()    
+    const SIZE = 10
+    var s = newSeq[Agent](SIZE)
+    for i in 0..<s.len:
+        s[i] = newAgent()
     
-    var pool = newSeq[FlowVar[Agent]](POOLSIZE)
-    parallel:
-        for i in 0..<pool.len:
-            pool[i] = spawn newAgent()
-    for i in 0..<pool.len:
-        let agent = (^pool[i])
-        echo agent.fitness
-    
-    echo getClockStr()        
-
+    echo $s[0]
+    mutate(s)
+    echo $s[0]
+  
 main()
