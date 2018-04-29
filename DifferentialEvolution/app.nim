758eaaeb2cdccf388296a09de0cab124a37002f6
Peter <peterhj@gmail.com>
2018-04-29 18:33:57 +0200

    use of arrays

--- DifferentialEvolution/app.nim
+++ DifferentialEvolution/app.nim
@@ -1,58 +1,84 @@
 import math
-import random
+# import random
 # import nimprof
 
 # randomize()
 
+# Constants
+const params = 2
+const print = 10000
+const generations = 1000
+const popsize = 100
+const mutate = 0.5
+
+# Optimization problems
+proc booth(x: array[params, float32]): float32 =
+    # (1.0, 3.0) = 0
+    let t1 = pow(x[0] + 2*x[1] - 7.0, 2.0)
+    let t2 = pow(2*x[0] + x[1] - 5.0, 2.0)
+    return t1 + t2  
+    
+const optimizer = booth
+
 # Helpers
-proc min_index(x: seq[float]): int =
-    var smallest = float.high
+proc min_index(x: array[popsize, float32]): int =
+    var smallest = float32.high
     for i in 0..<x.len:
         if x[i] < smallest:
             smallest = x[i]
             result = i
 
-# Optimization problems
-proc booth(x: seq[float]): float =
-    # (1.0, 3.0) = 0
-    let t1 = pow(x[0] + 2*x[1] - 7.0, 2.0)
-    let t2 = pow(2*x[0] + x[1] - 5.0, 2.0)
-    return t1 + t2    
+# Biased fast random
+var
+    x: uint32 = 123456789
+    y: uint32 = 362436069
+    z: uint32 = 521288629
+    w: uint32 = 88675123
+    
+proc xor128(): uint32 = 
+    var t: uint32
+    t = x xor (x shl 11)
+    x = y
+    y = z
+    z = w
+    w = w xor (w shr 19) xor t xor (t shr 8)
+    return w
 
-# Constants
-const print = 1000
-const generations = 10000
-const popsize = 10
-const params_range = -100.0..100.0
-const dither_range = 0.5..1.0
-const mutate = 0.5
-const optimizer = booth
-const params = 2
+proc f_rand(min, max: float32): float32 =
+    # A biased random!
+    let r = float32(xor128())
+    # pow(2, 32) - 1 == 4294967295
+    let rr = r / 4294967295.0
+    let rrr = rr * (max - min) + min
+    return rrr
+
+proc i_rand(min, max: uint32): int =
+    # A biased random
+    result = int((xor128() + min) mod max)
 
 var crossover = 0.9
-var scores = newSeq[float](popsize)
-var others = newSeq[int](3)
-var donor = newSeq[float](params)
-var trial = newSeq[float](params)
+var scores: array[popsize, float32]
+var others: array[3, int]
+var donor: array[params, float32]
+var trial: array[params, float32]
 
 # Init population
-var pop = newSeq[seq[float]](popsize)
+var pop: array[popsize, array[params, float32]]
 for i in 0..<popsize:
-    pop[i] = newSeq[float](params)
     for j in 0..<params:
-        pop[i][j] = rand(params_range)
+        pop[i][j] = f_rand(-100, 100)
     scores[i] = optimizer(pop[i])
 
 # For each generation
 for g in 0..<generations:
-    crossover = rand(dither_range)
+    crossover = f_rand(0.5, 1.0)
     
     for i in 0..<popsize:
         # Get three others
         for j in 0..<3:
-            var idx = rand(0..<popsize)
+            var idx = i_rand(0, popsize)
             while idx == i:
-                idx = rand(0..<popsize)
+                idx = i_rand(0, popsize)
             others[j] = idx
         
         let x0 = pop[others[0]]
@@ -68,7 +94,7 @@ for g in 0..<generations:
 
         # Create trial
         for j in 0..<params:
-            if rand(1.0) < crossover:
+            if f_rand(0, 1.0) < crossover:
                 trial[j] = donor[j]
             else:
                 trial[j] = xt[j]
