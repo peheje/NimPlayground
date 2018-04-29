c581381d58907066b00f201347ab0d58384331a0
Peter <peterhj@gmail.com>
2018-04-29 19:35:46 +0200

    wrap in main proc

--- DifferentialEvolution/app.nim
+++ DifferentialEvolution/app.nim
@@ -58,62 +58,65 @@ proc i_rand(min, max: uint32): int =
     # A biased random
     result = int((xor128() + min) mod max)
 
-var crossover = 0.9
-var scores: array[popsize, float]
-var others: array[3, int]
-var donor: array[params, float]
-var trial: array[params, float]
+proc main() =
+    var crossover = 0.9
+    var scores: array[popsize, float]
+    var others: array[3, int]
+    var donor: array[params, float]
+    var trial: array[params, float]
 
-# Init population
-var pop: array[popsize, array[params, float]]
-for i in 0..<popsize:
-    for j in 0..<params:
-        pop[i][j] = f_rand(boundFrom, boundTo)
-    scores[i] = optimizer(pop[i])
-
-# For each generation
-for g in 0..<generations:
-    crossover = f_rand(0.5, 1.0)
-    
+    # Init population
+    var pop: array[popsize, array[params, float]]
     for i in 0..<popsize:
-        # Get three others
-        for j in 0..<3:
-            var idx = i_rand(0, popsize)
-            while idx == i:
-                idx = i_rand(0, popsize)
-            others[j] = idx
-        
-        let x0 = pop[others[0]]
-        let x1 = pop[others[1]]
-        let x2 = pop[others[2]]
-        let xt = pop[i]
-
-        # Create donor
         for j in 0..<params:
-            donor[j] = x0[j] + (x1[j] - x2[j]) * mutate
-        
-        # Todo: EnsureBounds
+            pop[i][j] = f_rand(boundFrom, boundTo)
+        scores[i] = optimizer(pop[i])
 
-        # Create trial
-        for j in 0..<params:
-            if f_rand(0, 1.0) < crossover:
-                trial[j] = donor[j]
-            else:
-                trial[j] = xt[j]
+    # For each generation
+    for g in 0..<generations:
+        crossover = f_rand(0.5, 1.0)
         
-        # Greedy pick best
-        let score_trial = optimizer(trial)
-        let score_target = scores[i]
+        for i in 0..<popsize:
+            # Get three others
+            for j in 0..<3:
+                var idx = i_rand(0, popsize)
+                while idx == i:
+                    idx = i_rand(0, popsize)
+                others[j] = idx
+            
+            let x0 = pop[others[0]]
+            let x1 = pop[others[1]]
+            let x2 = pop[others[2]]
+            let xt = pop[i]
 
-        if score_trial < score_target:
+            # Create donor
             for j in 0..<params:
-                pop[i][j] = trial[j]
-            scores[i] = score_trial
-        
-    if g mod print == 0:
-        let mean = scores.sum() / scores.len().toFloat
-        echo "generation mean ", mean
-        echo "generation ", g
-    if g == generations-1:
-        let best_idx = scores.min_index()
-        echo "best ", pop[best_idx]
+                donor[j] = x0[j] + (x1[j] - x2[j]) * mutate
+            
+            # Todo: EnsureBounds
+
+            # Create trial
+            for j in 0..<params:
+                if f_rand(0, 1.0) < crossover:
+                    trial[j] = donor[j]
+                else:
+                    trial[j] = xt[j]
+            
+            # Greedy pick best
+            let score_trial = optimizer(trial)
+            let score_target = scores[i]
+
+            if score_trial < score_target:
+                for j in 0..<params:
+                    pop[i][j] = trial[j]
+                scores[i] = score_trial
+            
+        if g mod print == 0:
+            let mean = scores.sum() / scores.len().toFloat
+            echo "generation mean ", mean
+            echo "generation ", g
+        if g == generations-1:
+            let best_idx = scores.min_index()
+            echo "best ", pop[best_idx]
+
+main()
