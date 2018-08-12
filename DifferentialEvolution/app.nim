ae093b0dfd1da8d0f0067006af49a929500b9a85
Peter Helstrup Jensen <pehj@netcompany.com>
2018-08-12 14:42:48 +0200

    fast random into own file

--- DifferentialEvolution/app.nim
+++ DifferentialEvolution/app.nim
@@ -1,6 +1,7 @@
 import math
 import random
 import problems
+import fastrandom
 # import nimprof
 
 # Helpers
@@ -15,20 +16,6 @@ proc limit_bounds(x: var openarray[float], bound_from, bound_to: float) =
     for i in 0..<len(x):
         if x[i] < bound_from: x[i] = bound_from
         elif x[i] > bound_to: x[i] = bound_to
-
-var state = initRand(100)
-
-# A biased random
-proc f_rand(min, max: float): float =
-    let r = float(next(state))
-    # pow(2, 64) = 1.844674407E19
-    let rr = r / 1.844674407E19
-    let rrr = rr * (max - min) + min
-    return rrr
-
-# A biased random
-proc i_rand(min, max: uint64): int =
-    result = int((next(state) + min) mod max)
 
 proc main() =
     # Constants
