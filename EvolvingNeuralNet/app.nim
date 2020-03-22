ac05bc442ea2aa96c5d777ca9078ab270a29116c
Peter Helstrup Jensen <peterhj@gmail.com>
2020-03-22 16:57:34 +0100

    begun with the training functions

--- EvolvingNeuralNet/app.nim
+++ EvolvingNeuralNet/app.nim
@@ -2,6 +2,7 @@ import math
 import random
 import streams
 import json
+import os
 
 const bounds = -1.0..1.0
 
@@ -16,7 +17,10 @@ type
     
     Net* = ref object
         layers: seq[Layer]
-        weights: int
+        fitness: float
+
+func lerp(a, b, p: float): float =
+    return a + (b - a) * p
 
 proc newNeuron(nWeights: int): Neuron =
     new result
@@ -43,16 +47,62 @@ func invoke(x: Layer, input: seq[float]): seq[float] =
     for neuron in x.neurons:
         result.add(neuron.invoke(input, x.last))
 
+func invoke(x: Net, input: seq[float]): seq[float] =
+    result = input
+    for layer in x.layers:
+        result = layer.invoke(result)
+
+proc argMax(x: seq[float]): int =
+    var max = low(float)
+    result = -1
+    for i in 0..<x.len:
+        if x[i] > max:
+            max = x[i]
+            result = i
+
+proc correctPredictions(n: Net, xs: seq[seq[float]], ys: seq[int]): int =
+    result = 0
+    for i, x in xs:
+        let correct = ys[i]
+        let neuralGuess = n.invoke(x)
+        let bestGuess = argMax(neuralGuess)
+        if bestGuess == correct:
+            result += 1
+
+proc computeFitness(n: Net, xs: seq[seq[float]], ys: seq[int], parentInheritance: float, gamma: float) =
+    let correct = n.correctPredictions(xs, ys)
+    let correctFitness = float(correct) / float(xs.len)
+
+    var regularizationLoss = 0.0
+    if gamma != 0:
+        for layer in n.layers:
+            for neuron in layer.neurons:
+                for weight in neuron.weights:
+                    regularizationLoss += weight * weight
+    
+    let dataloss = 0.0 # todo
+
+    let batchFitness = correctFitness - regularizationLoss - dataloss
+    let parentFitness = n.fitness
+    n.fitness = parentInheritance * parentFitness + batchFitness
+
 proc newNet(setup: seq[int]): Net =
     new result
     let lastLayerIdx = setup.len - 2
     for i in 0..<setup.len-1:
         result.layers.add(newLayer(setup[i], setup[i+1], i == lastLayerIdx))
+    
+proc mutate(x: Neuron, power, frequency: float) =
+    for i in 0..<x.weights.len:
+        if rand(0.0..1.0) < frequency:
+            x.weights[i] += rand(-power..power)
+    if rand(0.0..1.0) < frequency:
+        x.bias += rand(-power..power)
 
-func invoke(x: Net, input: seq[float]): seq[float] =
-    result = input
+proc mutate(x: Net, power, frequency: float) =
     for layer in x.layers:
-        result = layer.invoke(result)
+        for neuron in layer.neurons:
+            neuron.mutate(power, frequency)
 
 proc writeJsonDebug(o: any) =
     const path = "/Users/phj/Desktop/nim_write.txt"
@@ -64,10 +114,14 @@ proc main() =
     randomize()
     let dataInputs = 3
     let dataOutputs = 4
-    let n1 = newNet(@[dataInputs, 100, 1000, 10000, 1000, 100, dataOutputs])
+    let n1 = newNet(
+        @[dataInputs, 100, 100, 100, 100, 100, dataOutputs])
     
     var result = n1.invoke(@[1.0, 2.0, 3.0])
-    
+    #writeJsonDebug(n1)
+    #sleep(2000)
+    for i in 0..<10:
+        n1.mutate(1.0, 0.25)
     #writeJsonDebug(n1)
     #echo result
 
