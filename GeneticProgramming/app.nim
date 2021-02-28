9177c74632d869baa35e26d8d2f296d274bac07b
Peter Helstrup Jensen <peterhj@gmail.com>
2021-02-28 22:36:09 +0100

    try power

--- GeneticProgramming/app.nim
+++ GeneticProgramming/app.nim
@@ -5,6 +5,10 @@ import sugar
 import strformat
 
 type
+
+    OperationClass = enum
+        value, infix, binary, unary
+
     Node = ref object
         left, right: Node
         op: Operation
@@ -12,15 +16,15 @@ type
 
     Operation = ref object
         sign: string
-        unary: bool
+        class: OperationClass
         eval: proc(a, b: Node): float
 
 proc eval(node: Node): float =
     if node == nil:
         return
-    elif node.op.sign == "val":
+    elif node.op.class == OperationClass.value:
         return node.value
-    elif node.op.unary:
+    elif node.op.class == OperationClass.unary:
         return node.op.eval(node.left, nil)
     return node.op.eval(node.left, node.right)
 
@@ -50,7 +54,7 @@ proc randomTree(ops: seq[Operation], node: var Node, max: int, counter: int = 0,
     if counter >= max:
         return
     randomTree(ops, node.left, max, counter + 1, node.op)
-    if not node.op.unary:
+    if node.op.class != OperationClass.unary:
         randomTree(ops, node.right, max, counter + 1, node.op)
 
 proc parenthesis(x: float): string =
@@ -65,15 +69,25 @@ proc toEquation(node: Node, eq: var string, depth: int = 0) =
     let left = node.left
 
     if right != nil and right.op.sign == "val":
-        eq &= fmt"({parenthesis(left.value)} {sign} {parenthesis(right.value)})"
-    elif not node.op.unary:
-        if depth != 0:
-            eq &= "("
-        left.toEquation(eq, depth + 1)
-        eq &= fmt" {sign} "
-        right.toEquation(eq, depth + 1)
-        if depth != 0:
+        if sign == "^":
+            eq &= fmt"pow({left.value}, {right.value})"
+        else:
+            eq &= fmt"({parenthesis(left.value)} {sign} {parenthesis(right.value)})"
+    elif node.op.class != OperationClass.unary:
+        if node.op.sign == "^":
+            eq &= "pow("
+            left.toEquation(eq, depth)
+            eq &= ", "
+            right.toEquation(eq, depth)
             eq &= ")"
+        else:
+            if depth != 0:
+                eq &= "("
+            left.toEquation(eq, depth + 1)
+            eq &= fmt" {sign} "
+            right.toEquation(eq, depth + 1)
+            if depth != 0:
+                eq &= ")"
     else:
         eq &= fmt"{sign}("
         if left.op.sign == "val":
@@ -88,8 +102,8 @@ proc main() =
 
     ops.add(Operation(sign: "val", eval: (a, b) => a.value))
     ops.add(Operation(sign: "+", eval: (a, b) => a.eval() + b.eval()))
-    ops.add(Operation(sign: "-", eval: (a, b) => a.eval() - b.eval()))
-    ops.add(Operation(sign: "*", eval: (a, b) => a.eval() * b.eval()))
+    #ops.add(Operation(sign: "-", eval: (a, b) => a.eval() - b.eval()))
+    #ops.add(Operation(sign: "*", eval: (a, b) => a.eval() * b.eval()))
     ops.add(Operation(sign: "^", eval: (a, b) => pow(a.eval(), b.eval())))
     #ops.add(Operation(sign: "/", eval: (a, b) => a.eval() / b.eval()))
     #ops.add(Operation(sign: "abs", eval: (a, b) => abs(a.eval()), unary: true))
@@ -99,12 +113,13 @@ proc main() =
 
     for i in 0..<1_000_000:
         var tree: Node = nil
-        randomTree(ops, tree, 5)
+        randomTree(ops, tree, 2)
         #tree.print()
 
-        if tree.eval() == 42.0:
+        if true or tree.eval() == 42.0:
             var equation = ""
             tree.toEquation(equation)
+            echo tree.eval()
             echo equation
             echo "_____"
 
