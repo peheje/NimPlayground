795bc52e5257c069bef8575611ec20ede3e4e298
PHJ <peterhj@gmail.com>
2021-02-28 15:24:35 +0100

    rename variable

--- GeneticProgramming/app.nim
+++ GeneticProgramming/app.nim
@@ -51,24 +51,24 @@ proc randomTree(ops: seq[Operation], node: var Node, max: int, counter: int = 0,
     if not node.op.unary:
         randomTree(ops, node.right, max, counter + 1, node.op)
 
-proc toEquation(node: Node, s: var string, depth: int = 0) =
+proc toEquation(node: Node, eq: var string, depth: int = 0) =
     let sign = node.op.sign
     let right = node.right
     let left = node.left
     
     if right != nil and right.op.sign == "val":
-        s &= fmt"({left.value} {sign} {right.value})"
+        eq &= fmt"({left.value} {sign} {right.value})"
     elif not node.op.unary:
-        if depth != 0: s &= "("
-        left.toEquation(s, depth + 1)
-        s &= fmt" {sign} "
-        right.toEquation(s, depth + 1)
-        if depth != 0: s &= ")"
+        if depth != 0: eq &= "("
+        left.toEquation(eq, depth + 1)
+        eq &= fmt" {sign} "
+        right.toEquation(eq, depth + 1)
+        if depth != 0: eq &= ")"
     else:
-        s &= fmt"{sign}("
-        if left.op.sign == "val": s &= fmt"{left.value}"
-        else: left.toEquation(s, depth + 1)
-        s &= ")"
+        eq &= fmt"{sign}("
+        if left.op.sign == "val": eq &= fmt"{left.value}"
+        else: left.toEquation(eq, depth + 1)
+        eq &= ")"
 
 proc main() =
     randomize()
@@ -87,14 +87,14 @@ proc main() =
     #operations.add(Operation(sign:"sqrt", unary: true, eval: (a, b) => sqrt(a.eval())))
     #operations.add(Operation(sign:"abs", unary: true, eval: (a, b) => abs(a.eval())))
 
-    for i in 0..<1:
+    for i in 0..<1_000_000:
         var tree = Node()
         randomTree(operations, tree, 5)
         var equation = ""
         tree.toEquation(equation)
 
         #tree.print()
-        echo equation
-        echo tree.eval()
+        #echo equation
+        #echo tree.eval()
 
 main()
