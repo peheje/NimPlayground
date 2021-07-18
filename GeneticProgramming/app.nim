e93dcf3729d1f331640be036bbcda2e423ce6fc5
peheje <peterhj@gmail.com>
2021-07-18 17:14:45 +0200

    use object variant pattern for genetic programming

--- GeneticProgramming/app.nim
+++ GeneticProgramming/app.nim
@@ -5,52 +5,70 @@ import sugar
 import strformat
 
 type
-    Node = ref object
-        left, right: Node
-        op: Operation
-        value: float
+    NodeKind = enum
+        nkValue,
+        nkBinary
+        nkUnary
 
-    Operation = ref object
+    Node = ref object
+        case kind: NodeKind
+        of nkValue: 
+            value: float
+        of nkBinary: 
+            binaryOperation: BinaryOperation
+            left, right: Node
+        of nkUnary:
+            unaryOperation: UnaryOperation
+            child: Node
+
+    BinaryOperation = ref object
         sign: string
-        unary: bool
         call: proc(a, b: Node): float
+
+    UnaryOperation = ref object
+        sign: string
+        call: proc(a: Node): float
 
 proc eval(node: Node): float =
     if node == nil:
         return
-    elif node.op.sign == "val":
+    elif node.kind == nkValue:
         return node.value
-    elif node.op.unary:
-        return node.op.call(node.left, nil)
-    return node.op.call(node.left, node.right)
+    elif node.kind == nkUnary:
+        return node.unaryOperation.call(node.child)
+    return node.binaryOperation.call(node.left, node.right)
 
 proc print(node: Node, indent: int = 0) =
     if node == nil:
         return
-    print(node.right, indent + 6)
+    if node.kind == nkBinary:
+        print(node.right, indent + 6)
     echo ""
     for i in 0..<indent:
         stdout.write(" ")
-    if node.op.sign == "val":
-        echo node.value.formatFloat(ffDecimal, 3)
-    else:
-        echo node.op.sign
-    print(node.left, indent + 6)
+    case node.kind
+        of nkValue:
+            echo node.value.formatFloat(ffDecimal, 3)
+        of nkBinary:
+            echo node.binaryOperation.sign
+        of nkUnary:
+            echo node.unaryOperation.sign
+    if node.kind == nkBinary:
+        print(node.left, indent + 6)
 
-proc randomNode(ops: seq[Operation], depth, max: int): Node =
+proc randomNode(ops: seq[BinaryOperation], depth, max: int): Node =
     let leaf = depth == max
-
     if leaf:
-        result = Node(op: ops[0], value: round(rand(-10.0..10.0)))
+        result = Node(kind: nkValue, value: round(rand(-10.0..10.0)))
     else:
-        result = Node(op: ops[rand(1..<ops.len)])
+        result = Node(kind: nkBinary, binaryOperation: ops[rand(0..<ops.len)])
 
-proc randomTree(ops: seq[Operation], node: var Node, max, counter: int = 0) =
+proc randomTree(ops: seq[BinaryOperation], node: var Node, max, counter: int = 0) =
     node = randomNode(ops, counter, max)
     if counter >= max:
         return
     randomTree(ops, node.left, max, counter + 1)
-    if not node.op.unary:
+    if node.kind != nkUnary:
         randomTree(ops, node.right, max, counter + 1)
 
 proc parenthesis(x: float): string =
@@ -60,13 +78,18 @@ proc parenthesis(x: float): string =
         return $x
 
 proc toEquation(node: Node, eq: var string, depth: int = 0) =
-    let sign = node.op.sign
     let right = node.right
     let left = node.left
 
-    if right != nil and right.op.sign == "val":
+    var sign = "UNINITIALIZED"
+    if node.kind == nkBinary:
+            sign = node.binaryOperation.sign
+    else:
+        assert(false)
+
+    if right != nil and right.kind == nkValue:
         eq &= fmt"({parenthesis(left.value)} {sign} {parenthesis(right.value)})"
-    elif not node.op.unary:
+    elif node.kind != nkUnary:
         if depth != 0:
             eq &= "("
         left.toEquation(eq, depth + 1)
@@ -76,7 +99,7 @@ proc toEquation(node: Node, eq: var string, depth: int = 0) =
             eq &= ")"
     else:
         eq &= fmt"{sign}("
-        if left.op.sign == "val":
+        if left.kind == nkValue:
             eq &= fmt"{left.value}"
         else:
             left.toEquation(eq, depth + 1)
@@ -84,25 +107,24 @@ proc toEquation(node: Node, eq: var string, depth: int = 0) =
 
 proc main() =
     randomize()
-    var ops = newSeq[Operation]()
+    var ops = newSeq[BinaryOperation]()
 
-    ops.add(Operation(sign: "val", call: (a, b) => a.value))
-    ops.add(Operation(sign: "+", call: (a, b) => a.eval() + b.eval()))
-    ops.add(Operation(sign: "-", call: (a, b) => a.eval() - b.eval()))
-    ops.add(Operation(sign: "*", call: (a, b) => a.eval() * b.eval()))
-    ops.add(Operation(sign: "^", call: (a, b) => pow(a.eval(), b.eval())))
+    ops.add(BinaryOperation(sign: "+", call: (a, b) => a.eval() + b.eval()))
+    ops.add(BinaryOperation(sign: "-", call: (a, b) => a.eval() - b.eval()))
+    ops.add(BinaryOperation(sign: "*", call: (a, b) => a.eval() * b.eval()))
+    ops.add(BinaryOperation(sign: "^", call: (a, b) => pow(a.eval(), b.eval())))
     #ops.add(Operation(sign: "/", call: (a, b) => a.eval() / b.eval()))
     #ops.add(Operation(sign: "abs", call: (a, b) => abs(a.eval()), unary: true))
     #ops.add(Operation(sign: "cos", call: (a, b) => cos(a.eval()), unary: true))
     #ops.add(Operation(sign: "sin", call: (a, b) => sin(a.eval()), unary: true))
     #ops.add(Operation(sign:"sqrt", call: (a, b) => sqrt(a.eval()), unary: true))
 
-    for i in 0..<1_000_000:
+    for i in 0..<100_000:
         var tree: Node = nil
-        randomTree(ops, tree, 5)
-        #tree.print()
+        randomTree(ops, tree, 2)
 
         if tree.eval() == 42.0:
+            tree.print()
             var equation = ""
             tree.toEquation(equation)
             echo equation
